b7c089c88d9685014f9091957b74766e891533ad
Vitalii Sashyn <vitaliis@wix.com>
2020-12-19 11:35:20 +0200

    Use children as code input to component

--- source/index.tsx
+++ source/index.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import {Text} from 'ink';
+import {highlight} from 'cli-highlight';
 
 /**
  * SyntaxHighlight.
  */
-const SyntaxHighlight = () => {
-	return <Text>hello</Text>;
+const SyntaxHighlight: React.FC = ({children}) => {
+	if (typeof children === 'string') {
+		return <Text>{highlight(children)}</Text>;
+	}
+	return null;
 };
 
 export default SyntaxHighlight;
