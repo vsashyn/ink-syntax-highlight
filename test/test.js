9d1c0fd4eecabcf775377a613ec1c0bb6a62be96
Vitalii Sashyn <vitaliis@wix.com>
2020-12-25 14:10:33 +0200

    Add test snapshot

---
+++ test/test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {render} from 'ink-testing-library';
+import test from 'ava';
+import SyntaxHighlight from '..';
+
+test('render code as in snapshot', async t => {
+	const {frames} = render(
+		<SyntaxHighlight code="const hello: string = 'world'" />
+	);
+	t.snapshot(frames);
+});
